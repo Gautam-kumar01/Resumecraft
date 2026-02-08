19dfe199f6ba22b6bbbc6cd462242bee531f84fd
Gautam-kumar01 <gautamkr192007@gmail.com>
2026-02-08 10:29:06 +0530

    Fix: Recreated api/index.js with correct UTF-8 encoding to resolve binary file issue

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-﻿module.exports = (req, res) => {
+module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
+
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
@@ -18,11 +18,11 @@
         const express = require('express');
         const app = express();
         app.use(express.json());
-        
+
         app.all('*', (req, res) => {
             res.json({ message: 'API is working', path: req.path });
         });
-        
+
         return app(req, res);
     } catch (error) {
         return res.status(500).json({
